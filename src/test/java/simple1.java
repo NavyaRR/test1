76de3a6fb22ba6792ea7fd654d76ddaf91fb2da1
Navya <navyanv2020@gmail.com>
2024-04-19 17:03:36 +0530

    my

---
+++ src/test/java/simple1.java
@@ -0,0 +1,9 @@
+
+public class simple1 {
+
+	public static void main(String[] args) {
+		
+	System.out.println("welcome");
+}
+
+}
---
+++ src/test/java/simple2.java
@@ -0,0 +1,8 @@
+
+public class simple2 {
+
+	public static void main(String[] args) {
+System.out.println("bye");
+	}
+
+}
